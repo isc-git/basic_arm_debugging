59f175f459dd89ffae9eef5093fa87d1ceafd66a
isc-GIT <isaacjcann@gmail.com>
2023-05-27 00:36:20 +1000

    feat(joints): added unlimited revolute

--- arm_lib/__init__.py
+++ arm_lib/__init__.py
@@ -1,4 +1,5 @@
 REVOLUTE_RADIUS: float = 0.2
+REVOLUTE_COMPLETE_COLOR: str = "#fceb88"
 REVOLUTE_POTENTIAL_COLOR: str = "#e6f2ec"
 REVOLUTE_FULFILLED_COLOR: str = "#b3ffd5"
 REVOLUTE_FULFILLED_ERROR: str = "red"
@@ -13,3 +14,4 @@ from .arm_lib import *
 from .arm import *
 from .limited_revolute_joint import *
 from .linear_element import *
+from .revolute_joint import *
